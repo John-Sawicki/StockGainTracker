6237ecdac759880481510ad6e74880b6977af0f9
john-sawicki <3Gxapnil>
2019-04-13 21:08:18 -0500

    static parse for currencies

--- app/src/main/java/com/example/android/stockgaintracker/MainActivity.kt
+++ app/src/main/java/com/example/android/stockgaintracker/MainActivity.kt
@@ -11,16 +11,37 @@ import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.example.android.stockgaintracker.Adapter.StockAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    val tag ="MainActivityLog"
     //TODO add currency api
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         rv_stocks.layoutManager = LinearLayoutManager(this)
         rv_stocks.adapter = StockAdapter(applicationContext)
-
+        Log.d(tag, "onCreate")
+        fetchJson()
     }
 
+    fun fetchJson(){
+        Log.d(tag, "fetchJson start")
+       println("fetch json start")  //info message
+        val url = "http://data.fixer.io/api/latest?access_key=0b8f3a332e80c3bf5bb94776ad121890"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object: Callback{
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body()?.string()
+                println("success json"+body)
 
+            }
+            override fun onFailure(call: Call, e: IOException) {
+
+                println("json fetch failed for stock values")  //info message
+            }
+        })
+    }
 }
